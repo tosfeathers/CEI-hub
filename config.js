87a9be204bba268daa89418902afb8aa44b0dd5b
tosfeathers <101580549+tosfeathers@users.noreply.github.com>
2024-07-22 10:57:31 -0400

    updated website text

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ var config = {
     theme: 'dark',
     use3dTerrain: true, //set true for enabling 3D maps.
     auto: false,
-    title: "Oregon's toxic time bomb",
+    title: "Oregon's Toxic Time Bomb",
     subtitle: "Oregon is due for a massive earthquake. When it comes, it will likely cause a toxic fuel spill as bad as Deepwater Horizon.",
     byline: 'By Todd Feathers',
     footer: 'Source: Data from the Oregon Department of Geology and Mineral Industries, the City of Portland, Multnomah County, the U.S. Geological Service, and Portland State University. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
@@ -20,7 +20,7 @@ var config = {
             alignment: 'left',
             hidden: false,
             title: 'The Megathrust',
-            description: 'On January 26, 1700, the tectonic Pacific Plate abruptly slipped further beneath the Juan de Fuca Plate, causing a "megathrust" earthquake about 70 miles off the West Coast of Oregon. The quake, which geological evidence suggests was around 8.7 to 9.2 magnitude on the Richter scale, destroyed low lying settlements, dropped the coastline of Oregon by several feet, and created a Tsunami that reached Japan. It was the last earthquake to occur on the 700 mile-long Cascadia Subduction Zone fault line. But seismologists say the next megathrust is coming due, with some simulations estimating a 37 percent chance it will occur in the next 50 years.',
+            description: 'On January 26, 1700, the tectonic Pacific Plate abruptly slipped further beneath the Juan de Fuca Plate, causing a "megathrust" earthquake about 70 miles off the West Coast of Oregon. The quake, which geological evidence suggests was around 8.7 to 9.2 magnitude on the Richter scale, destroyed low lying settlements, dropped the coastline of Oregon by several feet, and created a tsunami that reached Japan. It was the last earthquake to occur on the 700 mile-long Cascadia Subduction Zone fault line. But seismologists say the next megathrust is coming due, with some simulations estimating a 37 percent chance it will occur in the next 50 years.',
             location: {
                 center: [-129.48465, 46.84087],
                 zoom: 5,
@@ -48,7 +48,7 @@ var config = {
             id: 'second-identifier',
             alignment: 'left',
             hidden: false,
-            description: 'A magnitude 9 earthquake orginiating at the Cascadia Subduction Zone would cause destruction across the Pacific Northwest. The Oregon Department of Geology and Mineral Industries has <a href="https://www.multco.us/em/dogami-study-estimates-cascadia-earthquake-impacts-portland-region">estimated</a> it could result in $37 billion in damages, displace 85,000 people, and than cause 27,000 casualties, ranging from minor injuries to deaths. About 150 miles east from the fault line, in the city of Portland, the worst shaking will likely be along the banks of the Willamette River, where peak ground velocity could reach 44 centimeters per second, liquifying the soil.',
+            description: 'A magnitude 9.0 earthquake orginiating at the Cascadia Subduction Zone would cause destruction across the Pacific Northwest. The Oregon Department of Geology and Mineral Industries has <a href="https://www.multco.us/em/dogami-study-estimates-cascadia-earthquake-impacts-portland-region">estimated</a> it could result in $37 billion in damages, displace 85,000 people, and cause 27,000 casualties, ranging from minor injuries to deaths. About 150 miles east from the fault line, in the city of Portland, the worst shaking will likely be along the banks of the Willamette River, where peak ground velocity could reach 44 centimeters per second, liquifying the soil.',
             location: {
                 center: [-122.90501, 45.60760],
                 zoom: 11,
@@ -106,7 +106,7 @@ var config = {
             id: 'fourth-identifier',
             alignment: 'left',
             hidden: false,
-            description: "Sitting above this liquifaction zone is Oregon's Critical Energy Infrastructure Hub, which houses 630 tanks that store a variety of fuel and hazardous material. A 2020 study estimates that under the conditions caused by a megathrust earthquake, 397 of those tanks would fail, releasing between 94.6 million and 193.7 million gallons of gasoline, crude oil, and other toxic liquids into the ground and the Willamette River.",
+            description: "Sitting above this liquifaction zone is Oregon's Critical Energy Infrastructure Hub, which houses 630 tanks that store a variety of fuel and hazardous material. A 2022 study estimates that under the conditions caused by a megathrust earthquake, 397 of those tanks would fail, releasing between 94.6 million and 193.7 million gallons of gasoline, crude oil, and other toxic liquids into the ground and the Willamette River.",
             location: {
                 center: [-122.75195, 45.55684],
                 zoom: 13,
